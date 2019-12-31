7adb293f20bf46e4db5da681ded0fdebaa3bf019
jechol <jechol@chain.partners>
2019-12-31 17:06:46 +0900

    Improve test

--- witchcraft-examples/test/2_monad/80_state_tree_labeler_test.exs
+++ witchcraft-examples/test/2_monad/80_state_tree_labeler_test.exs
@@ -7,24 +7,26 @@ defmodule Example.StateTreeLabelerTest do
   alias Example.StateTreeLabeler, as: Labeler
 
   test "success cases" do
-    tree = Tree.new(9, nil, Tree.new(7, Tree.new(5)))
+    tree = Tree.new(:a, Tree.new(:b), Tree.new(:c, Tree.new(:d), Tree.new(:e)))
 
     # label from 0
-    {labeled_from_zero, 3} = tree |> Labeler.label_post_order() |> State.run(0)
+    {labeled_from_zero, 5} = tree |> Labeler.label_post_order() |> State.run(0)
 
     assert labeled_from_zero ==
              %Tree{
-               data: 2,
-               right: %Tree{data: 1, left: %Tree{data: 0}}
+               data: 4,
+               left: %Tree{data: 0},
+               right: %Tree{left: %Tree{data: 1}, right: %Tree{data: 2}, data: 3}
              }
 
     # label from 10
-    {labeled_from_ten, 13} = tree |> Labeler.label_post_order() |> State.run(10)
+    {labeled_from_ten, 15} = tree |> Labeler.label_post_order() |> State.run(10)
 
     assert labeled_from_ten ==
              %Tree{
-               data: 12,
-               right: %Tree{data: 11, left: %Tree{data: 10}}
+               data: 14,
+               left: %Tree{data: 10},
+               right: %Tree{left: %Tree{data: 11}, right: %Tree{data: 12}, data: 13}
              }
   end
 end
