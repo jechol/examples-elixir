df1a8379c83ef75f7cb3b80f4b146f4f7ec58876
jechol <jechol@chain.partners>
2020-01-02 06:33:26 +0900

    NaN

--- witchcraft-examples/test/1_functor/1_maybe_either_test.exs
+++ witchcraft-examples/test/1_functor/1_maybe_either_test.exs
@@ -15,6 +15,6 @@ defmodule Seminar.Functor.MaybeEitherTest do
     assert %Either.Right{right: 10} ~> (&(&1 * 2)) == %Either.Right{right: 20}
     assert %Either.Right{right: 10} ~> (&(&1 * 2)) ~> (&(&1 - 1)) == %Either.Right{right: 19}
 
-    assert %Either.Left{left: :NAN} ~> (&(&1 * 2)) == %Either.Left{left: :NAN}
+    assert %Either.Left{left: :NaN} ~> (&(&1 * 2)) == %Either.Left{left: :NaN}
   end
 end
