05d154539f30a1476ece516acb3760e913b645ef
jechol <jechol@chain.partners>
2020-01-02 12:04:57 +0900

    StateStackSafeCalc test done

---
+++ witchcraft-examples/lib/2_monad/82_state_stack_safe_calc.ex
@@ -0,0 +1,40 @@
+defmodule Example.StateStackSafeCalc do
+  use Witchcraft
+
+  alias Example.Expr.{Val, Div}
+  alias Algae.State
+  alias Algae.Either.{Left, Right}
+
+  import Algae.State
+
+  def eval(%Val{val: val}) do
+    monad %State{} do
+      modify fn stack -> [Right.new(val) | stack] end
+      return :ok
+    end
+  end
+
+  def eval(%Div{num: num, denom: denom}) do
+    monad %State{} do
+      :ok <- eval(denom)
+      :ok <- eval(num)
+
+      modify fn [num, denom | tail] ->
+        quotient =
+          monad %Right{} do
+            num <- num
+            denom <- denom
+
+            safe_div(num, denom)
+          end
+
+        [quotient | tail]
+      end
+
+      return :ok
+    end
+  end
+
+  defp safe_div(_, 0), do: Left.new(:div_by_zero)
+  defp safe_div(n, m), do: Right.new(n / m)
+end
---
+++ witchcraft-examples/test/2_monad/82_state_stack_safe_calc_test.exs
@@ -0,0 +1,39 @@
+defmodule Example.StateStackSafeCalcTest do
+  use ExUnit.Case
+  use Witchcraft
+
+  alias Algae.State
+  alias Algae.Either.{Left, Right}
+  alias Example.Expr.{Val, Div}
+  alias Example.StateStackSafeCalc, as: Calc
+
+  test "success val" do
+    assert Val.new(2) |> Calc.eval() |> State.run([]) == {:ok, [%Right{right: 2}]}
+  end
+
+  test "success div" do
+    assert Div.new(
+             Val.new(4),
+             Val.new(2)
+           )
+           |> Calc.eval()
+           |> State.run([]) == {:ok, [%Right{right: 2}]}
+
+    assert Div.new(
+             Val.new(10),
+             Div.new(Val.new(4), Val.new(2))
+           )
+           |> Calc.eval()
+           |> State.run([]) == {:ok, [%Right{right: 5}]}
+  end
+
+  test "failure cases" do
+    assert Div.new(
+             Val.new(10),
+             Val.new(0)
+           )
+           |> Calc.eval()
+           |> State.run([]) ==
+             {:ok, [%Left{left: :div_by_zero}]}
+  end
+end
