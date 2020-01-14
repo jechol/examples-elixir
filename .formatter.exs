f5688c8c53423f08534e6d604a95fe495a004aea
jechol <jechol@chain.partners>
2020-01-15 08:43:39 +0900

    MaybeBind

--- .formatter.exs
+++ .formatter.exs
@@ -1,4 +1,16 @@
 # Used by "mix format"
 [
-  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"]
+  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
+  locals_without_parens: [
+    let: 1,
+    tell: 1,
+    get: 1,
+    put: 1,
+    modify: 1,
+    return: 1,
+    defdata: 1,
+    defdata: 3,
+    defsum: 1,
+    defalias: 2
+  ]
 ]
