6a58fb3621418dd0b3100eb45d097ac05e36b23d
jechol <jechol@chain.partners>
2020-01-15 08:16:25 +0900

    Naive -> Unsafe

--- test/10_unsafe_calc_test.exs
+++ test/10_unsafe_calc_test.exs
@@ -1,20 +1,22 @@
-defmodule NaiveCalcTest do
+defmodule UnsafeCalcTest do
   use ExUnit.Case
 
   alias Expr.{Val, Div}
 
   test "success case" do
+    # 6 / (4 / 2) = 3
     assert Val.new(6)
            |> Div.new(Div.new(Val.new(4), Val.new(2)))
-           |> NaiveCalc.eval() ==
+           |> UnsafeCalc.eval() ==
              3
   end
 
   test "div by zero" do
+    # 1 / 0
     assert_raise ArithmeticError, fn ->
       Val.new(1)
       |> Div.new(Val.new(0))
-      |> NaiveCalc.eval()
+      |> UnsafeCalc.eval()
     end
   end
 end
