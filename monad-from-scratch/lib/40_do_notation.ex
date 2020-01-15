1dcd8fd952685e79a043138eecc9b5c2664a9a7f
jechol <jechol@chain.partners>
2020-01-15 13:43:11 +0900

    Add explanation for convert_ast

--- monad-from-scratch/lib/40_do_notation.ex
+++ monad-from-scratch/lib/40_do_notation.ex
@@ -14,6 +14,16 @@ defmodule DoNotation do
           unquote(right) |> Bind.bind(fn unquote(left) -> unquote(acc) end)
         end
     end)
+
+    # Above Enum.reduce() works like following.
+    #
+    # 1. elem : b_val <- eval(b)
+    #    acc  : safe_div(a_val, b_val)
+    # => eval(b) |> Bind.bind(fn b_val -> safe_div(a_val, b_val) end)
+    #
+    # 2. elem : a_val <- eval(a)
+    #    acc  : eval(b) |> Bind.bind(fn b_val -> safe_div(a_val, b_val) end)
+    # => eval(a) |> Bind.bind(fn a_val -> eval(b) |> Bind.bind(fn b_val -> safe_div(a_val, b_val) end) end)
   end
 
   def example_from do
