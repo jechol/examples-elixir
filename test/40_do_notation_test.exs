07ff2f1268a93bb4ceb3ba64173b63e6e7ca9312
jechol <jechol@chain.partners>
2020-01-15 10:20:19 +0900

    rename vars

--- test/40_do_notation_test.exs
+++ test/40_do_notation_test.exs
@@ -4,14 +4,14 @@ defmodule DoNotationTest do
   require DoNotation
 
   test "do_notation" do
-    from =
+    from_ast =
       quote do
         a_val <- eval(a)
         b_val <- eval(b)
         safe_div(a_val, b_val)
       end
 
-    to =
+    to_ast =
       quote do
         eval(a)
         |> Bind.bind(fn a_val ->
@@ -22,8 +22,8 @@ defmodule DoNotationTest do
         end)
       end
 
-    converted = DoNotation.convert_ast(from)
+    converted_ast = DoNotation.convert_ast(from_ast)
 
-    assert Macro.to_string(converted) == Macro.to_string(to)
+    assert Macro.to_string(converted_ast) == Macro.to_string(to_ast)
   end
 end
