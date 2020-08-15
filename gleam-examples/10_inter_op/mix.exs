dc4d8b76a4bb78a0d3c46551c9de89c707c157c7
Jechol Lee <mr.jechol@gmail.com>
2020-08-15 19:08:35 +0900

    Add rebar.config

--- gleam-examples/10_inter_op/mix.exs
+++ gleam-examples/10_inter_op/mix.exs
@@ -23,7 +23,8 @@ defmodule InterOp.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:mix_gleam, ">= 0.0.0"}
+      {:mix_gleam, ">= 0.0.0"},
+      {:gleam_stdlib, ">= 0.0.0"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
---
+++ gleam-examples/10_inter_op/test/gleam/gleam_calc_test.gleam
@@ -0,0 +1,10 @@
+import gleam_calc
+import gleam/should
+import gleam/atom
+
+pub fn add_test() {
+  // assert gleam_calc.add(1, 2) == 3
+  "ok"
+  |> atom.from_string
+  |> should.be_error
+}
