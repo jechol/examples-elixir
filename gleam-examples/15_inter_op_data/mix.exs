210e04961ea110dcdc491794afd20383f428af0d
Jechol Lee <mr.jechol@gmail.com>
2020-08-15 20:17:07 +0900

    rebar3 eunit

--- gleam-examples/15_inter_op_data/mix.exs
+++ gleam-examples/15_inter_op_data/mix.exs
@@ -7,7 +7,9 @@ defmodule InterOpData.MixProject do
       version: "0.1.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      erlc_paths: ["src", "gen"],
+      compilers: [:gleam | Mix.compilers()]
     ]
   end
 
