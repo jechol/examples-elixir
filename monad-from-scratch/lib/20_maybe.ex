245c39f2450415c827dc1779942be52ba04a9c5b
Jechol Lee <mr.jechol@gmail.com>
2022-08-30 13:28:59 +0900

    remove nil from Just

--- monad-from-scratch/lib/20_maybe.ex
+++ monad-from-scratch/lib/20_maybe.ex
@@ -2,7 +2,7 @@ defmodule Maybe do
   defmodule Just do
     defstruct [:just]
 
-    def new(val \\ nil), do: %Just{just: val}
+    def new(val), do: %Just{just: val}
   end
 
   defmodule Nothing do
